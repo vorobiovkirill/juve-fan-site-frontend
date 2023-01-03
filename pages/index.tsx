b2909fd500a9cc68a8e12560b956242537766bb6
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-03 21:07:33 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Juve Fan Site</title>
+        <meta name="description" content="Juventus Ukraine fan site" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>JUVE UA</h1>
+      </main>
+    </>
+  )
+}
