9a046e6660b4b2512120f3f61476f3965b2caa2b
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-04 20:22:52 +0200

    add @graphql-codegen

--- components/News.tsx
+++ components/News.tsx
@@ -1,25 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
+import { NewsPostsDocument } from '../generated/graphql'
 
-const GET_NEWS = gql`
-query NewsPosts {
-  newsposts {
-    data {
-      attributes {
-        title
-        writtenBy
-        imageUrl
-        publishedAt
-        createdAt
-        updatedAt
-      }
-    }
-  }
-}
-`
 
 export const News = () => {
-    const { data, loading } = useQuery(GET_NEWS)
-    return (
-        <div>{JSON.stringify(data, null, 2)}</div>
-    )
+  const { data, loading } = useQuery(NewsPostsDocument)
+  return (
+    <div>{JSON.stringify(data, null, 2)}</div>
+  )
 }
---
+++ graphql.codegen.ts
@@ -0,0 +1,16 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: 'http://0.0.0.0:1337/graphql',
+  documents: ['graphql/**/*.graphql'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  // watch: true,
+  generates: {
+    './generated/': {
+      preset: 'client',
+      plugins: ['typescript', 'typescript-operations']
+    }
+  }
+}
+
+export default config
