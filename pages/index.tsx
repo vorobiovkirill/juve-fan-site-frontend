f503dcfbce1d3d713f491ec5e1b57f2e99856fed
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-04 17:47:37 +0200

    add flowbite, path alias, components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import Head from 'next/head'
-import { News } from '../components/News'
+import { Footer } from 'components/Footer'
+import { Header } from 'components/Header'
+import { News } from 'components/News'
 
 export default function Home() {
   return (
-    <>
+    <div className="p-0 m-0 min-h-screen grid grid-rows-[auto_1fr_auto]">
       <Head>
         <title>Juve Fan Site</title>
         <meta name="description" content="Juventus Ukraine fan site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <main>
-        <h1>JUVE UA</h1>
+        CONTENT
         <News />
       </main>
-    </>
+      <Footer />
+    </div>
   )
 }
