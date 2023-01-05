0de4c09eff564ccca85d44deff87240e9e00fd9f
Kirill Vorobiov <mishkacod@gmail.com>
2023-01-05 17:05:21 +0200

    add google font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,9 +2,13 @@ import React, { PropsWithChildren } from 'react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
-export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+type LayoutProps = {
+    className: string
+}
+
+export const Layout: React.FC<PropsWithChildren & LayoutProps> = ({ className, children }) => {
     return (
-        <div className="p-0 m-0 min-h-screen grid grid-rows-[auto_1fr_auto]">
+        <div className={className}>
             <Header />
             <main className="grid grid-cols-3 gap-x-4">{children}</main>
             <Footer />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import '../styles/globals.css'
 import type { AppContext, AppInitialProps } from 'next/app'
+import { Inter } from '@next/font/google'
 import { ApolloProvider } from "@apollo/client"
 import { useApollo } from "@/lib/apollo-client"
 import { Layout } from '@/components/Layout'
+
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppContext & AppInitialProps) {
   const apolloClient = useApollo(pageProps);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Layout>
+      <Layout className={`${inter.variable} font-sans p-0 m-0 min-h-screen grid grid-rows-[auto_1fr_auto]`}>
         <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
